0df6f30ec540a61011106d69c6f8cd5a51639e55
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-02-28 18:44:14 +0300

    Initial codebase for the project has been implemented.  - Libraries are added.  - Scala 2.11.8 / SBT 1.1.1 / Spark 2.2.1

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]) {
+    //val logFile = "YOUR_SPARK_HOME/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+    //val logData = spark.read.textFile(logFile).cache()
+    //val numAs = logData.filter(line => line.contains("a")).count()
+    //val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a, Lines with b")
+    spark.stop()
+  }
+}
