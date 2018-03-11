4f4844908dbcc6eefd6e773a052a334fdfa38ca5
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-03-11 21:38:57 +0300

    Intersection functions are implemented.
    - Able to get unified node as a seperate node.

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -1,2 +1,41 @@
+import scala.collection.mutable
 
-case class Node(id: String, followingList: Map[String, Int] = null)
+case class Node(id: String, followingList: Map[String, Int] = null, subNodes: Option[Set[String]] = None)
+
+object Node {
+
+  def getIntersectedNode(a: Node, b: Node): Node ={
+    val intersectionSet = getIntersectionSet(a, b)
+    unifyNodes(a, b ,intersectionSet)
+  }
+
+  def unifyNodes(a: Node, b: Node, intersectionSet: Set[String]): Node ={
+    val tempIntersection = mutable.Map.empty[String, Int]
+
+    intersectionSet.foreach { case (id: String) =>
+      val aCount = a.followingList.getOrElse(id, 0)
+      val bCount = b.followingList.getOrElse(id, 0)
+
+      tempIntersection.put(id, aCount + bCount + 1)
+    }
+    Node(
+      a.id,
+      a.followingList ++ b.followingList ++ tempIntersection,
+      Option(
+        Set(b.id, a.id) ++ a.subNodes.getOrElse(Set.empty)
+      ))
+  }
+
+  def getIntersectionSet(a: Node, b: Node): Set[String] ={
+    val unifiedMap = a.followingList ++ b.followingList
+
+    val onlyNonIntersectedB = b.followingList -- a.followingList.keySet
+    val onlyNonIntersectedA = a.followingList -- b.followingList.keySet
+
+    val aExcluded = unifiedMap -- onlyNonIntersectedA.keySet
+    val allExcluded = aExcluded -- onlyNonIntersectedB.keySet
+
+    allExcluded.keySet
+  }
+
+}
---
+++ src/test/scala/NodeTest.scala
@@ -0,0 +1,42 @@
+import org.scalatest.{FunSuite, Matchers}
+
+class NodeTest extends FunSuite
+  with Matchers
+  with SparkTestWrapper {
+
+  test("Gets the true intersection set") {
+    val aNode = Node("10", Map[String, Int]("30" -> 1, "40" -> 1, "50" -> 1, "100" -> 5))
+    val bNode = Node("20", Map[String, Int]("30" -> 1, "60" -> 1, "70" ->1, "100" -> 2))
+
+    val intersectionSet = Node.getIntersectionSet(aNode, bNode)
+
+    intersectionSet.size should equal(2)
+    intersectionSet should contain("30")
+    intersectionSet should contain("100")
+  }
+
+  test("Returns empty node if there is no intersection") {
+    val aNode = Node("10", Map[String, Int]("150" -> 1, "40" -> 1, "50" -> 1, "110" -> 5))
+    val bNode = Node("20", Map[String, Int]("30" -> 1, "60" -> 1, "70" ->1, "100" -> 2))
+
+    val intersectionSet = Node.getIntersectionSet(aNode, bNode)
+
+    intersectionSet.size should equal(0)
+  }
+
+  test("Returns unified node") {
+    val aNode = Node("10", Map[String, Int]("30" -> 1, "40" -> 1, "50" -> 1, "100" -> 2))
+    val bNode = Node("20", Map[String, Int]("30" -> 1, "60" -> 1, "70" ->1, "100" -> 3))
+    val intersectionSet = Set("30", "100")
+
+    val unifiedNode = Node.unifyNodes(aNode, bNode, intersectionSet)
+
+    unifiedNode.followingList.size should equal(6)
+    unifiedNode.followingList("30") should equal(3)
+    unifiedNode.followingList("100") should equal(6)
+    unifiedNode.followingList("40") should equal(1)
+    unifiedNode.followingList("50") should equal(1)
+    unifiedNode.followingList("60") should equal(1)
+    unifiedNode.followingList("70") should equal(1)
+  }
+}
